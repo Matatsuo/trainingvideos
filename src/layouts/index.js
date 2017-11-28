c2ac2eee2e50c1d4d1eca6e42e5159128bcd7fb2
Jordan McRae <imjordanm@gmail.com>
2017-11-28 14:58:04 +1300

    added branding, navigation and new pages

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,64 +1,39 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import Link from 'gatsby-link'
-import Helmet from 'react-helmet'
+import React from "react";
+import PropTypes from "prop-types";
+import Link from "gatsby-link";
+import Helmet from "react-helmet";
+import { Header, Navigation } from "../components/header";
+import logo from "../resources/images/medimap.png";
 
-import '../styles/main.scss';
-
-const Header = () => (
-  <header
-    style={{
-      backgroundColor: 'rgb(20, 15, 15)',
-      borderBottom: 'solid 1px white',
-      height: '10vh',
-      alignItems: 'center',
-    display: 'flex',
-    flexDirection: 'column',
-    justifyContent: 'center',
-    }}
-  >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: 'white',
-            textDecoration: 'none',
-            textTransform: 'uppercase',
-          }}
-        >
-          Company Name
-        </Link>
-      </h1>
-  </header>
-)
+import "../styles/main.scss";
 
 const TemplateWrapper = ({ children }) => (
   <div>
     <Helmet
-      title="Gatsby Default Starter"
+      title="Medi Map Training"
       meta={[
-        { name: 'description', content: 'Sample' },
-        { name: 'keywords', content: 'sample, something' },
+        { name: "description", content: "Sample" },
+        { name: "keywords", content: "sample, something" }
       ]}
     />
     <Header />
     <main
       style={{
-        margin: '0 auto',
-        textAlign: 'center',
-        height: '90vh',
-        alignItems: 'center',
-    display: 'flex',
-    justifyContent: 'center',
+        margin: "0 auto",
+        textAlign: "center",
+        minHeight: "80vh",
+        alignItems: "center",
+        display: "flex",
+        justifyContent: "center"
       }}
     >
       {children()}
     </main>
   </div>
-)
+);
 
 TemplateWrapper.propTypes = {
-  children: PropTypes.func,
-}
+  children: PropTypes.func
+};
 
-export default TemplateWrapper
+export default TemplateWrapper;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,10 @@
-import React from 'react'
-import Link from 'gatsby-link'
+import React from "react";
+import Link from "gatsby-link";
 
 const IndexPage = () => (
   <div>
-    <iframe className="ytPlayer" width="560" height="315" src="https://www.youtube-nocookie.com/embed/videoseries?list=PL0VqShxGcrNQZxKC63dGIa0fxN7oK6cO_&amp;rel=0&amp;modestbranding=1" frameborder="0" modestbranding="1" rel="0" showinfo="0" gesture="media" allowfullscreen></iframe>
-    <p>To view the demonstrations simply press play to start watching the full playlist.</p>
-    <p> To go back or forward a video, push the previous or next buttons. Alternatively, you can view and select any video from the playlist by clicking the playlist button at the top left of the video player. </p>
+    <h2 className="pageHeader">Medi Map Training</h2>
   </div>
-)
+);
 
-export default IndexPage
+export default IndexPage;
