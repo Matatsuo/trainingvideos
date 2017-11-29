1c3af87c777a2d21a3b9f4c5551bcf3343a912f6
Jordan McRae <imjordanm@gmail.com>
2017-11-29 21:23:23 +1300

    fixed page layout

--- src/templates/page.js
+++ src/templates/page.js
@@ -1,39 +1,39 @@
-import React from "react";
+import React from 'react';
 
 export default function Template({ data }) {
   const { markdownRemark: page } = data;
   return (
     <div className="container">
-      <nav className="sidebar">
+      <div className="sidebar">
         <ul>
           {page.frontmatter.videos
             ? page.frontmatter.videos.map(vid => <li>{vid.video.title}</li>)
             : null}
         </ul>
-      </nav>
+      </div>
 
-      <div>
+      <div className="content">
         <h1 className="pageHeader">{page.frontmatter.title}</h1>
         <div>
           {page.frontmatter.videos
             ? page.frontmatter.videos.map(vid => (
-                <div>
-                  <h2>{vid.video.title}</h2>
-                  <iframe
-                    className="ytPlayer"
-                    width="560"
-                    height="315"
-                    src={vid.video.link}
-                    frameborder="0"
-                    gesture="media"
-                    allowfullscreen
-                  />
-                </div>
+              <div>
+                <h2>{vid.video.title}</h2>
+                <iframe
+                  className="ytPlayer"
+                  width="560"
+                  height="315"
+                  src={vid.video.link}
+                  frameBorder="0"
+                  gesture="media"
+                  allowFullScreen
+                />
+              </div>
               ))
             : null}
         </div>
       </div>
-      <div />
+      <div className="sidebar" />
     </div>
   );
 }
