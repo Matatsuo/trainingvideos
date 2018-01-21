fa6b0fa414b2c238642c31ac8b4188cfe5d3c982
Jordan McRae <imjordanm@gmail.com>
2018-01-22 05:59:38 +1300

    Merge branch 'master' of https://github.com/imjordanm/trainingvideos

--- src/pages/404.js
+++ src/pages/404.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const NotFoundPage = () => (
   <div className="page-container">
-    <h1 className="page-header landing">This page does not exist</h1>
+    <h1 className="page-header landing">Page does not exist</h1>
     <div className="landing-container">
       <div className="landing-description">
         <p>Try clicking on one of the pages above to get back on track.</p>
